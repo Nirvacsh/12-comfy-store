e530e975545cdcb8fc228b1b258ab515c6066016
Nirvacsh <nir.vlad.zu@gmail.com>
2023-11-03 18:15:23 +0200

    third commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,26 @@
 const About = () => {
-  return <div>About</div>
+  return (
+    <>
+      <div className='flex fex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
+        <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+          We love
+        </h1>
+        <div className='stats bg-primary shadow'>
+          <div className='stat'>
+            <div className='stat-title text-primary-content text-4xl font-bold tracking-widest'>
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className='mt-6 text-lg leading-8 max-w-2xl mx-auto'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod suscipit
+        exercitationem dignissimos sint est nemo non, ab quidem, magni
+        reprehenderit a quibusdam atque, explicabo quisquam dolor? Provident
+        deserunt eligendi placeat corporis! Fuga, vel repellendus? Natus eius
+        enim ipsum quam incidunt!
+      </p>
+    </>
+  )
 }
 export default About
